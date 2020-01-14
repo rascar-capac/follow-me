73b315b01f30a5e511122b83cf600c1269da801a
VitalRenock <55354524+VitalRenock@users.noreply.github.com>
2020-01-14 19:52:34 +0100

    Remove Tribe For PlayGround_scene
    Remove Tribe For PlayGround_scene
    Add script: AseetsSpawnerOnTerrain (in progress)

--- Assets/R&D/Scripts/AssetsSpawnerOnTerrain.cs
+++ Assets/R&D/Scripts/AssetsSpawnerOnTerrain.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class AssetsSpawnerOnTerrain : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	[Header("Box Options")]
+	public Vector3 _boxSize = new Vector3(10, 10, 10);
+	public Color _boxColor = Color.green;
+	[Header("Assets Options")]
+	public List<GameObject> _prefabsList;
+	[Header("Terrain Options")]
+	public Terrain _terrain;
+	TerrainData _terrainData;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	float[,] _terrainHeights;
+
+	private void Awake()
+	{
+		_terrainData = _terrain.terrainData;
+		_terrainHeights = _terrainData.GetHeights(0, 0, (int)_terrainData.size.x, (int)_terrainData.size.y);
+	}
+
+	void SpawnPrefabsOnTerrain()
+	{
+		Instantiate(_prefabsList[Random.Range(0, _prefabsList.Count)]);
+	}
+	private void OnDrawGizmos()
+	{
+		Gizmos.color = _boxColor;
+		Gizmos.DrawWireCube(transform.position, _boxSize);
+	}
 }
