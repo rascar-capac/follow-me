c4e57d48f3cc30773fb0dc7e9e75d01f6529cf38
Delto-ID <o.rosan@skynet.be>
2020-01-15 15:26:42 +0100

    Layer and physics matrix deactivation

--- Assets/Scripts/Player/PlayerPlaceBeacon.cs
+++ Assets/Scripts/Player/PlayerPlaceBeacon.cs
@@ -5,14 +5,15 @@ using UnityEngine.AI;
 
 public class PlayerPlaceBeacon : BaseMonoBehaviour
 {
-	NavMeshAgent _tribeAgent;
-    Camera _mainCamera;
     public GameObject _tribeFollowTargetPrefab;
+    public float DropDistance = 1.0f;
 
     GameObject _tribeFollowTarget;
     RaycastHit _hitInfo = new RaycastHit();
+    NavMeshAgent _tribeAgent;
+    Camera _mainCamera;
 
-	private void Start()
+    private void Start()
 	{
         _tribeAgent = ((GameObject)ObjectsManager.I["Tribe"]).GetComponent<NavMeshAgent>();
         _mainCamera = CameraManager.I._MainCamera;
@@ -24,12 +25,12 @@ public class PlayerPlaceBeacon : BaseMonoBehaviour
 
     protected void Update()
     {
-        Debug.DrawRay(_mainCamera.transform.position + _mainCamera.transform.forward, Vector3.down * 100.0f, Color.black);
+        Debug.DrawRay(_mainCamera.transform.position + _mainCamera.transform.forward * DropDistance, Vector3.down * 100.0f, Color.black);
     }
 
     void PlaceBeacon()
     {
-        if (Physics.Raycast(_mainCamera.transform.position + _mainCamera.transform.forward, Vector3.down, out _hitInfo, 100.0f))
+        if (Physics.Raycast(_mainCamera.transform.position + _mainCamera.transform.forward * DropDistance, Vector3.down, out _hitInfo, 100.0f))
         {
             _tribeFollowTarget.transform.position = _hitInfo.point;
             _tribeAgent.destination = _tribeFollowTarget.transform.position;
