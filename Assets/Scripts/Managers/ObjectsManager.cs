0b20625a1befa6c4376459d13e04ccf528e42766
Delto-ID <o.rosan@skynet.be>
2020-01-15 13:32:34 +0100

    New scenes Camera & lights

--- Assets/Scripts/Managers/ObjectsManager.cs
+++ Assets/Scripts/Managers/ObjectsManager.cs
@@ -6,6 +6,6 @@ public class ObjectsManager : Singleton<ObjectsManager>
 {
     protected void Start()
     {
-        this["Tribe"] = GameObject.Find("Tribe");  
+        this["Tribe"] = GameObject.Find("Tribe");
     }
 }
