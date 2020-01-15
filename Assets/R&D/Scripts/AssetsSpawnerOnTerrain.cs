ff27ef325a0de5abf1e09bfa4af12e79c8d72223
VitalRenock <55354524+VitalRenock@users.noreply.github.com>
2020-01-15 13:08:31 +0100

    Clean PlayGround_scene + Script AssetsSpawner
    Clean PlayGround_scene + Script AssetsSpawner

--- Assets/R&D/Scripts/AssetsSpawnerOnTerrain.cs
+++ Assets/R&D/Scripts/AssetsSpawnerOnTerrain.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
+using Sirenix.OdinInspector;
 
 public class AssetsSpawnerOnTerrain : MonoBehaviour
 {
@@ -12,30 +14,41 @@ public class AssetsSpawnerOnTerrain : MonoBehaviour
 	public int _spawnCount;
 	[Header("Terrain Options")]
 	public Terrain _terrain;
+	[Range(0, 1)] public float _groundOffset = 0;
 	TerrainData _terrainData;
 
-	float[,] _terrainHeights;
 
-	private void Awake()
+	private void OnEnable()
 	{
-		_terrainData = _terrain.terrainData;
-		_terrainHeights = _terrainData.GetHeights(0, 0, (int)_terrainData.size.x, (int)_terrainData.size.y);
+		_terrainData = _terrain.terrainData;	
 	}
 
-	
+	private void OnValidate()
+	{
+		_terrainData = _terrain.terrainData;
+	}
+
+	[Button("Spawn")]
 	void SpawnPrefabsOnTerrain()
 	{
-		Instantiate(_prefabsList[Random.Range(0, _prefabsList.Count)], RandomPosition(), Quaternion.identity, transform);
+		for (int i = 0; i < _spawnCount; i++)
+		{
+			Instantiate(_prefabsList[Random.Range(0, _prefabsList.Count)], RandomPosition(), Quaternion.identity, transform);
+		}
 	}
 
 	Vector3 RandomPosition()
 	{
+		float xRandomPos = Random.Range(transform.position.x, transform.position.x + _boxSize.x);
+		float zRandomPos = Random.Range(transform.position.z, transform.position.z + _boxSize.z);
+		float yPos = _terrainData.GetHeight((int)xRandomPos, (int)zRandomPos) + (_prefabsList[0].transform.lossyScale.y);
 
-		return new Vector3(_boxSize.x, 0 , _boxSize.z);
+		return new Vector3(xRandomPos, yPos , zRandomPos);
 	}
+
 	private void OnDrawGizmos()
 	{
 		Gizmos.color = _boxColor;
-		Gizmos.DrawWireCube(transform.position, _boxSize);
+		Gizmos.DrawWireCube(transform.position + (_boxSize / 2), _boxSize);
 	}
 }
