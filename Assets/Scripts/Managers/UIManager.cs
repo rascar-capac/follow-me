74f8ee79443c7613fbfe3edca877efd01e07fa16
VitalRenock <55354524+VitalRenock@users.noreply.github.com>
2020-01-17 15:32:38 +0100

    Clean UIManager
    Clean code UIManager
    + Rename UI Elements

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -5,34 +5,44 @@ using UnityEngine.UI;
 
 public class UIManager : Singleton<UIManager>
 {
-    public List<GameObject> Menus;
-    public Text DistanceText;
-    public Text TimeText;
-    public Text AlertText;
-    PlayerMovement player;
-
-	public GameObject _backgroundImage;
-	public GameObject _inventoryPanel;
-    public GameObject _content;
-
-    RectTransform _inventoryContent;
-	public GameObject _inventoryCellPrefab;
-	public float _inventoryCellHeight;
-	List<GameObject> _inventoryUiItemList = new List<GameObject>();
+	#region References Player
+    PlayerMovement _refPlayerMovement;
 	PlayerInventory _refPlayerInventory;
+	#endregion
+
+	#region References UI Elements
+	[Header("UI Réferences")]
+	public GameObject _inventoryPanel;
+    public GameObject _inventoryContent;
+	public GameObject _hudPanel;
+    public Text _HudTribeDistanceText;
+    public Text _HudAlertMessageText;
+    public Text _HudCurrentTimeText;
+	public GameObject _backgroundImage;
+	#endregion
+
+	#region UI HUD Variables
+	public List<GameObject> Menus;
+	#endregion
+
+	#region Prefabs UI Elements
+	[Header("Assets Réferences")]
+	public GameObject _inventoryCellPrefab;
+	#endregion
+
+	#region UI Inventory Variables
+	List<GameObject> _inventoryUiItemList = new List<GameObject>();
+	#endregion
+
 
 	protected override void Start()
 	{
 		base.Start();
-        player = ((GameObject)ObjectsManager.I["Player"]).GetComponent<PlayerMovement>();
+
+        _refPlayerMovement = ((GameObject)ObjectsManager.I["Player"]).GetComponent<PlayerMovement>();
+		_refPlayerInventory = ((GameObject)ObjectsManager.I["Player"]).GetComponent<PlayerInventory>();
+
 		InputManager.I.onOpenInventoryKeyPressed.AddListener(OpenInventory);
-
-		_refPlayerInventory = GameObject.FindObjectOfType<PlayerInventory>();
-
-        //_backgroundImage = GameObject.Find("BackgroundImage");
-        //_inventoryPanel = GameObject.Find("InventoryPanel");
-        _inventoryContent = _content.GetComponent<RectTransform>();// GameObject.Find("InventoryPanel/Scroll View/Viewport/Content").GetComponent<RectTransform>();
-		_inventoryCellHeight = _inventoryCellPrefab.GetComponent<RectTransform>().sizeDelta.y;
 
 		_backgroundImage.SetActive(false);
 		_inventoryPanel.SetActive(false);
@@ -45,6 +55,9 @@ public class UIManager : Singleton<UIManager>
 		// Inventory Open
 		if (!_inventoryPanel.activeSelf)
 		{
+			RectTransform invContentRectT = _inventoryContent.GetComponent<RectTransform>();
+			float invCellHeight = _inventoryCellPrefab.GetComponent<RectTransform>().sizeDelta.y;
+
 			//PauseGame(true);
 			_backgroundImage.SetActive(true);
 			_inventoryPanel.SetActive(true);
@@ -53,12 +66,12 @@ public class UIManager : Singleton<UIManager>
 			Cursor.lockState = CursorLockMode.None;
 
 			// Set height of InventoryPanel/Scroll View/Viewport/Content
-			_inventoryContent.sizeDelta = new Vector2(_inventoryContent.sizeDelta.x, _inventoryCellHeight * _refPlayerInventory._playerInventory.Count);
+			invContentRectT.sizeDelta = new Vector2(invContentRectT.sizeDelta.x, invCellHeight * _refPlayerInventory._playerInventory.Count);
 
 			// Instantiate all cells for each items in inventory and set-up cell
 			for (int i = 0; i < _refPlayerInventory._playerInventory.Count; i++)
 			{
-				_inventoryUiItemList.Add(Instantiate(_inventoryCellPrefab, _inventoryContent));
+				_inventoryUiItemList.Add(Instantiate(_inventoryCellPrefab, invContentRectT));
 
 				_inventoryUiItemList[i].transform.GetChild(0).GetComponent<Image>().sprite = _refPlayerInventory._playerInventory[i]._itemIcon;
 				_inventoryUiItemList[i].transform.GetChild(1).GetComponent<Text>().text = _refPlayerInventory._playerInventory[i]._itemName;
@@ -100,13 +113,13 @@ public class UIManager : Singleton<UIManager>
     #region Show messages
     public void SetTribeDistance()
     {
-        DistanceText.text = $"Tribe distance " + player.TribeDistance + " m";
-        if (player.IsTooFar)
-            DistanceText.text += " - Too far.";
+        _HudTribeDistanceText.text = $"Tribe distance " + _refPlayerMovement.TribeDistance + " m";
+        if (_refPlayerMovement.IsTooFar)
+            _HudTribeDistanceText.text += " - Too far.";
     }
     public void SetTimeOfDay()
     {
-        TimeText.text = $"Current time " + (int)AmbiantManager.I.CurrentTimeOfDay + " h (" + AmbiantManager.I.CurrentDayState.State.ToString() + ")";
+        _HudCurrentTimeText.text = $"Current time " + (int)AmbiantManager.I.CurrentTimeOfDay + " h (" + AmbiantManager.I.CurrentDayState.State.ToString() + ")";
     }
 
     private void Update()
@@ -117,13 +130,13 @@ public class UIManager : Singleton<UIManager>
 
     public void AlertMessage(string message)
     {
-        AlertText.text = message;
-        AlertText.gameObject.SetActive(true);
+        _HudAlertMessageText.text = message;
+        _HudAlertMessageText.gameObject.SetActive(true);
         StartChrono(3, DisableMessage);
     }
     void DisableMessage()
     {
-        AlertText.gameObject.SetActive(false);
+        _HudAlertMessageText.gameObject.SetActive(false);
     }
     #endregion
 }
