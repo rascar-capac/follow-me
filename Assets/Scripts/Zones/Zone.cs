e3a8750eef1bdbe50e321b714b62ca689b20be43
Delto-ID <o.rosan@skynet.be>
2020-01-17 16:56:00 +0100

    Show player life / Create zone prefab with script

---
+++ Assets/Scripts/Zones/Zone.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zone : BaseMonoBehaviour
+{
+    [Header("Zone radius")]
+    public float Radius = 5f;
+    [Header("Zone radius")]
+    public bool AllowCompass = false;
+}
