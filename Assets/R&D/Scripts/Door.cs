cf096f7faffe0b337d14cd3f31c657a6ccc6ffe2
VitalRenock <55354524+VitalRenock@users.noreply.github.com>
2020-01-25 21:21:45 +0100

    Update Inspector GameData
    Update Inspector GameData
    + RD Door

---
+++ Assets/R&D/Scripts/Door.cs
@@ -0,0 +1,205 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+[System.Serializable]
+public struct OpeningDoorProperties
+{
+	public DayState DayState;
+	public List<Vector2> OpeningHoursInDayState;
+}
+
+public class Door : BaseMonoBehaviour
+{
+	[Title("Door")]
+	public float DoorClosedAngle = 0f;
+	public float DoorOpenAngle = 90f;
+	public float OpeningSpeed = 5f;
+
+	[Title("Colliders size")]
+	public float XSizeColliders = 1f;
+	public float YSizeColliders = 3f;
+	public float ZSizeColliders = 0.3f;
+
+	[ToggleGroup("DayStateMode")]
+	public bool DayStateMode = false;
+	[ToggleGroup("DayStateMode")]
+	public List<DayState> OpeningDayStates;
+	[ToggleGroup("DayStateMode")]
+	public List<DayState> ClosingDayStates;
+
+	[ToggleGroup("TimeOfDayMode")]
+	public bool TimeOfDayMode = false;
+	[ToggleGroup("TimeOfDayMode")]
+	public List<OpeningDoorProperties> DoorOpeningHours;
+	
+
+	[FoldoutGroup("DebugMode")][ShowInInspector][ReadOnly]
+	Transform LeftDoor;
+	[FoldoutGroup("DebugMode")][ShowInInspector][ReadOnly]
+	Transform RightDoor;
+	[FoldoutGroup("DebugMode")][ShowInInspector][ReadOnly]
+	BoxCollider LeftCollider;
+	[FoldoutGroup("DebugMode")][ShowInInspector][ReadOnly]
+	BoxCollider RightCollider;
+	[FoldoutGroup("DebugMode")][ShowInInspector][ReadOnly]
+	public bool isOpen = false;
+
+	Coroutine InOpeningDoor;
+	Coroutine InClosingDoor;
+
+	private void OnValidate()
+	{
+		#region  Checking prefab references for script
+		if (LeftDoor == null)
+			LeftDoor = transform.GetChild(1);
+		if (RightDoor == null)
+			RightDoor = transform.GetChild(2);
+		if (LeftCollider == null)
+			LeftCollider = LeftDoor.GetComponentInChildren<BoxCollider>();
+		if (RightCollider == null)
+			RightCollider = RightDoor.GetComponentInChildren<BoxCollider>();
+		#endregion
+
+		#region Change closed door angle with inspector
+		SetDoorCurrentAngle(DoorClosedAngle);
+		#endregion
+
+		#region Change colliders size with inspector
+		LeftCollider.size = new Vector3(XSizeColliders, YSizeColliders, ZSizeColliders);
+		LeftCollider.center = new Vector3(XSizeColliders / 2, YSizeColliders / 2, LeftCollider.center.z);
+		LeftDoor.localPosition = new Vector3(-XSizeColliders, LeftDoor.localPosition.y, LeftDoor.localPosition.z);
+
+		RightCollider.size = new Vector3(XSizeColliders, YSizeColliders, ZSizeColliders);
+		RightCollider.center = new Vector3(-XSizeColliders / 2, YSizeColliders /2, RightCollider.center.z);
+		RightDoor.localPosition = new Vector3(XSizeColliders, RightDoor.localPosition.y, RightDoor.localPosition.z);
+		#endregion
+	}
+
+	private void Update()
+	{
+		Debug.Log("--------------");
+		if (GamePaused)
+			return;
+
+		if (DayStateMode == true)
+			DoorDayStateMode();
+
+		if (TimeOfDayMode == true)
+			DoorTimeOfDayMode();
+
+		//Debug.Log(Mathf.Round(AmbiantManager.I.CurrentTimeOfDay) + "H");
+		Debug.Log("Actual Timer = " + AmbiantManager.I.CurrentTimeOfDay);
+		Debug.Log("Actual State = " + AmbiantManager.I.CurrentDayState.State.ToString());
+		Debug.Log("--------------");
+	}
+
+	public void DoorTimeOfDayMode()
+	{
+		bool _IsOpeningHour = false;
+		//DayState _CurrentDayState = AmbiantManager.I.CurrentDayState.State;
+		float _CurrentTime = AmbiantManager.I.CurrentTimeOfDay;
+
+		Debug.Log("Count = " + DoorOpeningHours.Count);
+
+		for (int i = 0; i < DoorOpeningHours.Count; i++)
+		{
+			Debug.Log("--- " + DoorOpeningHours[i].DayState.ToString());
+
+			//if (DoorOpeningHours[i].DayState != AmbiantManager.I.CurrentDayState.State/*_CurrentDayState*/)
+			//	break;
+
+			//foreach (Vector2 _DoorOpeningHour in DoorOpeningHours[i].OpeningHoursInDayState)
+			//	if (_CurrentTime >= _DoorOpeningHour.x && _CurrentTime < _DoorOpeningHour.y)
+			//		_IsOpeningHour = true;
+
+			if (DoorOpeningHours[i].DayState == AmbiantManager.I.CurrentDayState.State/*_CurrentDayState*/)
+			{
+				foreach (Vector2 _DoorOpeningHour in DoorOpeningHours[i].OpeningHoursInDayState)
+				if (_CurrentTime >= _DoorOpeningHour.x && _CurrentTime < _DoorOpeningHour.y)
+					_IsOpeningHour = true;
+			}
+		}
+
+		if (!isOpen && _IsOpeningHour)
+			OpenDoor();
+		else if (isOpen && !_IsOpeningHour)
+			CloseDoor();
+	}
+
+	public void DoorDayStateMode()
+	{
+		if (!isOpen && OpeningDayStates.Contains(AmbiantManager.I.CurrentDayState.State))
+			OpenDoor();
+
+		else if (isOpen && ClosingDayStates.Contains(AmbiantManager.I.CurrentDayState.State))
+			CloseDoor();
+	}
+
+	public void SetDoorCurrentAngle(float currentAngle)
+	{
+		LeftDoor.transform.eulerAngles = new Vector3(0, currentAngle, 0);
+		RightDoor.transform.eulerAngles = new Vector3(0, -currentAngle, 0);
+	}
+
+	[Button("Open Door (Playmode only)")]
+	public void OpenDoor()
+	{
+		if (InOpeningDoor == null && InClosingDoor == null)
+			InOpeningDoor = StartCoroutine(OpeningDoor());
+		else
+			Debug.Log("Animation en cours!");
+	}
+	IEnumerator OpeningDoor()
+	{
+		float _DoorCurrentAngle = DoorClosedAngle;
+
+		while (_DoorCurrentAngle < DoorOpenAngle)
+		{
+			_DoorCurrentAngle += OpeningSpeed * Time.deltaTime;
+
+			if (_DoorCurrentAngle > DoorOpenAngle)
+				_DoorCurrentAngle = DoorOpenAngle;
+
+			SetDoorCurrentAngle(_DoorCurrentAngle);
+
+			yield return null;
+		}
+
+		Debug.Log("Fin Ouverture");
+		isOpen = true;
+		InOpeningDoor = null;
+		yield return null;
+	}
+
+	[Button("Close Door (Playmode only)")]
+	public void CloseDoor()
+	{
+		if (InOpeningDoor == null && InClosingDoor == null)
+			InClosingDoor = StartCoroutine(ClosingDoor());
+		else
+			Debug.Log("Animation en cours!");
+	}
+	IEnumerator ClosingDoor()
+	{
+		float _DoorCurrentAngle = DoorOpenAngle;
+
+		while (_DoorCurrentAngle > DoorClosedAngle)
+		{
+			_DoorCurrentAngle -= OpeningSpeed * Time.deltaTime;
+
+			if (_DoorCurrentAngle < DoorClosedAngle)
+				_DoorCurrentAngle = DoorClosedAngle;
+
+			SetDoorCurrentAngle(_DoorCurrentAngle);
+
+			yield return null;
+		}
+
+		Debug.Log("Fin Fermeture");
+		isOpen = false;
+		InClosingDoor = null;
+		yield return null;
+	}
+}
--- Assets/Scripts/ScriptableObjects/GameData.cs
+++ Assets/Scripts/ScriptableObjects/GameData.cs
@@ -6,96 +6,100 @@ using Sirenix.OdinInspector;
 [CreateAssetMenu(fileName = "Game Parameters", menuName = "New game LD", order = 1)]
 public class GameData : ScriptableObject
 {
-	[Title("Controls parameters")]
-    [Tooltip("Camera rotation speed")]
+	[Title("Camera")]
+	[TabGroup("Player")][Tooltip("Camera rotation speed")]
     public float CameraRotationSpeed = 100f;
 
-    [Space(2)]
-    [Title("Player Energy")]
-	[Tooltip("The energy of the player when the game starts.")]
+	[Title("Energy")]
+	[TabGroup("Player")][Tooltip("The energy of the player when the game starts.")]
 	public float InitialPlayerEnergy = 100f;
-	[Tooltip("Energy player lost per second in the day")]
+	[TabGroup("Player")][Tooltip("Energy player lost per second in the day")]
 	public float EnergyPlayerLostPerSecond = 1f;
-	[Tooltip("Energy player gain per second in the night")]
+	[TabGroup("Player")][Tooltip("Energy player gain per second in the night")]
 	public float EnergyPlayerGainPerSecond = 1f;
 
-    [Title("Player Speed")]
-	[Tooltip("The speed of the player when the game starts.")]
+	[Title("Speed")]
+	[TabGroup("Player")][Tooltip("The speed of the player when the game starts.")]
     public float InitialPlayerSpeed = 12.0f;
-    [Tooltip("speed multiplicator value when running (Speed * SpeedMultiplicator = Run).")]
+	[TabGroup("Player")][Tooltip("speed multiplicator value when running (Speed * SpeedMultiplicator = Run).")]
     public float SpeedMultiplicator = 2f;
-
-    [Title("Player Items interactions")]
-    [Tooltip("The distance minimum betwees player and item for interaction.")]
-    public float PlayerItemDistanceInteraction = 2f;
-    [Tooltip("When the player may activate the stones.")]
-    public List<DayState> StonesActivationUsable;
-
-    [Title("Tools parameters")]
-    [Tooltip("The maximum distance between player and tribe allowing beacon drop")]
-    public float MaximumDistanceOfTribe = 300.0f;
-    [Tooltip("when compass is usable")]
-    public List<DayState> CompassUsable;
-    [Tooltip("compass is unusable when energy low")]
-    public bool CompassEnergyLowUnusable;
-
-    [Tooltip("when beacon placer is usable")]
-    public List<DayState> BeaconPlacerUsable;
-    [Tooltip("beacon placer is unusable when energy low")]
-    public bool BeaconPlacerEnergyLowUnusable;
-    [Tooltip("distance where the beacon is dropped by the player")]
-    public float BeaconPlacementDistance = 3f;
-
-    [Tooltip("when map is usable")]
-    public List<DayState> MapUsable;
-
-    [Tooltip("when the player may run")]
+	[TabGroup("Player")][Tooltip("when the player may run")]
     public List<DayState> PlayerRunUsable;
-    [Tooltip("Player run is unusable when energy low")]
+	[TabGroup("Player")][Tooltip("Player run is unusable when energy low")]
     public bool PlayerRunEnergyLowUnusable;
-    [Tooltip("Energy player cost per second when running")]
+	[TabGroup("Player")][Tooltip("Energy player cost per second when running")]
     public float EnergyPlayerRunCostPerSecond = 1f;
 
+    [Title("Items interactions")]
+	[TabGroup("Player")][Tooltip("The distance minimum betwees player and item for interaction.")]
+    public float PlayerItemDistanceInteraction = 2f;
+	[TabGroup("Player")][Tooltip("When the player may activate the stones.")]
+    public List<DayState> StonesActivationUsable;
 
-    [Tooltip("when sundial is usable")]
+	[Title("Compass")]
+	[TabGroup("Tools")][Tooltip("when compass is usable")]
+    public List<DayState> CompassUsable;
+	[TabGroup("Tools")][Tooltip("compass is unusable when energy low")]
+    public bool CompassEnergyLowUnusable;
+
+	[Title("Beacon")]
+	[TabGroup("Tools")][Tooltip("when beacon placer is usable")]
+    public List<DayState> BeaconPlacerUsable;
+	[TabGroup("Tools")][Tooltip("beacon placer is unusable when energy low")]
+    public bool BeaconPlacerEnergyLowUnusable;
+	[TabGroup("Tools")][Tooltip("The maximum distance between player and tribe allowing beacon drop")]
+    public float MaximumDistanceOfTribe = 300.0f;
+	[TabGroup("Tools")][Tooltip("distance where the beacon is dropped by the player")]
+    public float BeaconPlacementDistance = 3f;
+
+	[Title("Map")]
+	[TabGroup("Tools")][Tooltip("when map is usable")]
+    public List<DayState> MapUsable;
+
+	[Title("Sun dial")]
+	[TabGroup("Tools")][Tooltip("when sundial is usable")]
     public List<DayState> SundialUsable;
-    [Tooltip("sundial is unusable when energy low")]
+	[TabGroup("Tools")][Tooltip("sundial is unusable when energy low")]
     public bool SundialEnergyLowUnusable;
 
-    [Tooltip("when player energy meter is usable")]
+	[Title("Energy meter")]
+	[TabGroup("Tools")][Tooltip("when player energy meter is usable")]
     public List<DayState> PlayerEnergyMeterUsable;
-    [Tooltip("player energy meter is unusable when energy low")]
+	[TabGroup("Tools")][Tooltip("player energy meter is unusable when energy low")]
     public bool PlayerEnergyMeterEnergyLowUnusable;
 
-    [Tooltip("when tribe energy meter is usable")]
+	[Title("Energy meter")]
+	[TabGroup("Tools")][Tooltip("when tribe energy meter is usable")]
     public List<DayState> TribeEnergyMeterUsable;
-    [Tooltip("tribe energy meter is unusable when energy low")]
+	[TabGroup("Tools")][Tooltip("tribe energy meter is unusable when energy low")]
     public bool TribeEnergyMeterEnergyLowUnusable;
 
-    [Tooltip("when tribe distance meter is usable")]
+	[Title("Distance meter")]
+	[TabGroup("Tools")][Tooltip("when tribe distance meter is usable")]
     public List<DayState> TribeDistanceMeterUsable;
-    [Tooltip("tribe distance meter is unusable when energy low")]
+	[TabGroup("Tools")][Tooltip("tribe distance meter is unusable when energy low")]
     public bool TribeDistanceMeterEnergyLowUnusable;
 
-    [Title("Tribe parameters")]
-	[Tooltip("The energy of the tribe when the game starts.")]
+    [Title("Tribe energy")]
+	[TabGroup("Tribe")][Tooltip("The energy of the tribe when the game starts.")]
 	public float InitialTribeEnergy = 100f;
-	[Tooltip("Percent of energy for entering in critical state")]
-	[Range(0, 100)]
+	[TabGroup("Tribe")][Tooltip("Percent of energy for entering in critical state")][Range(0, 100)]
 	public float PercentEnergyTribeForCritical = 5f;
-	[Tooltip("Energy tribe lost per second in the day")]
+	[TabGroup("Tribe")][Tooltip("Energy tribe lost per second in the day")]
 	public float EnergyTribeLostPerSecond = 1f;
-	[Tooltip("Energy tribe gain per second in the night")]
+	[TabGroup("Tribe")][Tooltip("Energy tribe gain per second in the night")]
 	public float EnergyTribeGainPerSecond = 1f;
-	[Tooltip("Default speed of Tribe")]
+
+	[Title("Tribe speed")]
+	[TabGroup("Tribe")][Tooltip("Default speed of Tribe")]
 	public float InitialSpeedTribe = 20f;
-	[Tooltip("Critical (energy low) speed multiplicator of Tribe (speed * multiplicator = new speed)")]
+	[TabGroup("Tribe")][Tooltip("Critical (energy low) speed multiplicator of Tribe (speed * multiplicator = new speed)")]
 	public float CriticalSpeedTribeMultiplicator = 0.5f;
-    [Tooltip("Default speed rotation of Tribe")]
+	[TabGroup("Tribe")][Tooltip("Default speed rotation of Tribe")]
     public float InitialSpeedRotationTribe = 10f;
 
-    [Title("Ambiance parameters")]
-    [Tooltip("Day states (night and day)")]
+    [Title("Ambience parameters")]
+	[TabGroup("Ambience")][Tooltip("Day states (night and day)")]
     public DayStatesProperties[] States;
 }
 
