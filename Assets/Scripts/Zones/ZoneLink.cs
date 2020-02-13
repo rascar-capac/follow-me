bcd1eb5fcd749f253b6f2eb082a06764c9925947
Delto-ID <o.rosan@skynet.be>
2020-02-13 09:36:20 +0100

    Zones link

---
+++ Assets/Scripts/Zones/ZoneLink.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoneLink : Zone
+{
+    public ZoneEgg LinkedZone;
+    Player player;
+    Tribe tribe;
+
+    protected override void Start()
+    {
+        base.Start();
+        player = ((GameObject)ObjectsManager.I["Player"]).GetComponent<Player>();
+        player.onZoneEnter.AddListener(EnteredZone);
+        tribe = ((GameObject)ObjectsManager.I["Tribe"]).GetComponent<Tribe>();
+    }
+
+    public void EnteredZone(ZoneInteractable who, Zone zone)
+    {
+        if (zone == this)
+        {
+            LinkedZone.gameObject.SetActive(true);
+            tribe.SetMode(TribeEmotionMode.Happy);
+            StartCoroutine(tribe.RotatingAround(LinkedZone.Egg.transform).GetEnumerator());
+        }
+    }
+}
